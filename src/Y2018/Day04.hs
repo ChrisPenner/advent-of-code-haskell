95f9a2aa345073522709b9f082b2a64b147eaef4
Chris Penner <christopher.penner@gmail.com>
2018-12-05 14:42:10 -0600

    Clean up day 4

--- src/Y2018/Day04.hs
+++ src/Y2018/Day04.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE ViewPatterns #-}
 module Y2018.Day04 where
 
 import Text.Megaparsec hiding (State)
@@ -14,24 +13,24 @@ import Control.Lens hiding (noneOf)
 import Data.List as L
 import qualified Data.Map as M
 
-type TimeMap = M.Map Int (M.Map Int (Sum Int))
-data GuardState = GuardState { _activeGuard :: Int
-                             , _awake :: Bool
-                             , _startedSleep :: LocalTime
+type Parser = Parsec () String
+type GuardID = Int
+type Minute = Int
+type MinuteMap = M.Map Minute (Sum Int)
+type TimeMap = M.Map GuardID MinuteMap
+data GuardState = GuardState { _activeGuard :: GuardID
+                             , _startedSleep :: Minute
                              , _timeMap :: TimeMap
                              }
 makeLenses ''GuardState
 
 startState :: GuardState
 startState = GuardState
-  { _activeGuard  = (-1)
-  , _awake        = True
+  { _activeGuard  = undefined
   , _startedSleep = undefined
   , _timeMap      = mempty
   }
 
-
-type Parser = Parsec () String
 
 data Action = Asleep | WokeUp | NewShift Int
   deriving Show
@@ -47,7 +46,6 @@ parseLine = do
   space
   action' <- choice [newShift, wakesUp, fallAsleep]
   time'   <- parseTimeM True defaultTimeLocale "%Y-%-m-%-d %H:%M" timeString
-  void $ many (noneOf "\n")
   return $ Log time' action'
  where
   wakesUp    = WokeUp <$ string "wakes up"
@@ -57,61 +55,55 @@ parseLine = do
     guardID <- read <$> many digitChar
     return (NewShift guardID)
 
+collectLogs :: [Log] -> TimeMap
+collectLogs = _timeMap . flip execState startState . traverse processLog
+
+processLog :: Log -> State GuardState ()
+processLog (Log logTime action') = do
+  let currentMin = todMin . localTimeOfDay $ logTime
+  case action' of
+    NewShift i -> activeGuard .= i
+    WokeUp     -> do
+      startedSleeping <- use startedSleep
+      logSleepingTime startedSleeping currentMin
+    Asleep -> startedSleep .= currentMin
+
+logSleepingTime :: Minute -> Minute -> State GuardState ()
+logSleepingTime minStart minEnd = forM_ [minStart .. (minEnd - 1)] $ \t -> do
+  guardID <- use activeGuard
+  timeMap . at guardID . non mempty . at t . non mempty <>= Sum 1
+
+buildTimeMap :: IO TimeMap
+buildTimeMap = do
+  input <- readFile "./input/2018-04.txt"
+  let parsedLines =
+        traverse (parseMaybe (parseLine <* many anyChar)) $ lines input
+  logs <- maybe (fail "failed to parse") pure parsedLines
+  let sortedLogs = sortOn time logs
+  let timeMap'   = collectLogs sortedLogs
+  return timeMap'
+
+getMostConsistentlySleepy :: TimeMap -> (GuardID, Minute)
+getMostConsistentlySleepy m =
+  let (guardID, sleepTimes) =
+        maximumBy (compare `on` maximum . snd) . M.toList $ m
+  in  (guardID, getSleepiestMinute sleepTimes)
+
+getSleepiestGuard :: TimeMap -> (GuardID, MinuteMap)
+getSleepiestGuard = maximumBy (compare `on` fold . snd) . M.toList
+
+getSleepiestMinute :: MinuteMap -> Minute
+getSleepiestMinute = fst . maximumBy (compare `on` snd) . M.toList
+
 part1 :: IO ()
 part1 = do
   timeMap' <- buildTimeMap
-  let sleepiestGuard  = getSleepiestGuard timeMap'
-  let sleepiestMinute = getSleepiestMinute <$> M.lookup sleepiestGuard timeMap'
-  print ((sleepiestGuard *) <$> sleepiestMinute)
+  let (sleepiestGuard, minuteMap) = getSleepiestGuard timeMap'
+  let sleepiestMinute             = getSleepiestMinute minuteMap
+  print $ sleepiestGuard * sleepiestMinute
 
 part2 :: IO ()
 part2 = do
   timeMap' <- buildTimeMap
   let (guardID, sleepyMinute) = getMostConsistentlySleepy timeMap'
-  print $ (guardID, sleepyMinute)
   print $ guardID * sleepyMinute
-
-getMostConsistentlySleepy :: TimeMap -> (Int, Int)
-getMostConsistentlySleepy m =
-  let
-    listOfGuardPatterns :: [(Int, M.Map Int (Sum Int))]
-    listOfGuardPatterns = M.toList m
-    (guardID, sleepTimes) =
-      maximumBy (compare `on` maximum . snd) listOfGuardPatterns
-    sleepiestMinute = fst . maximumBy (compare `on` snd) $ M.toList sleepTimes
-  in
-    (guardID, sleepiestMinute)
-
-buildTimeMap :: IO TimeMap
-buildTimeMap = do
-  input <- readFile "./input/2018-04.txt"
-  let parsedLines = traverse (parseMaybe parseLine) $ lines input
-  logs <- maybe (fail "failed to parse") pure $ parsedLines
-  let sortedLogs = sortOn time logs
-  let timeMap'   = collectLogs sortedLogs
-  return timeMap'
-
-collectLogs :: [Log] -> TimeMap
-collectLogs logs =
-  _timeMap . flip execState startState . forM logs $ \(Log logTime action') ->
-    do
-      case action' of
-        NewShift i -> activeGuard .= i
-        WokeUp     -> do
-          awake .= True
-          startedSleeping <- use startedSleep
-          logSleepingTime startedSleeping logTime
-        Asleep -> awake .= False >> startedSleep .= logTime
-
-logSleepingTime :: LocalTime -> LocalTime -> State GuardState ()
-logSleepingTime (localTimeOfDay -> todMin -> minStart) (localTimeOfDay -> todMin -> minEnd)
-  = forM_ [minStart .. (minEnd - 1)] $ \t -> do
-    guardID <- use activeGuard
-    timeMap . at guardID . non mempty . at t . non mempty <>= Sum 1
-
-getSleepiestGuard :: TimeMap -> Int
-getSleepiestGuard m =
-  fst . maximumBy (compare `on` snd) . M.toList $ fmap fold m
-
-getSleepiestMinute :: M.Map Int (Sum Int) -> Int
-getSleepiestMinute m = fst . maximumBy (compare `on` snd) . M.toList $ m
